549c7c8990141371bfbc864ea7a24864859ee46a
DevMarg <gurau.margarita@gmail.com>
2024-03-13 15:16:41 +0100

    Add several objects to the array
    If applied, this commit will add objects to the array with the same keys (name, height, types)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 let pokemonList = [];
+pokemonList = [
+    {name: 'Fearow', height: 1.2 , types: ['normal', 'flying']},
+    {name: 'Butterfree', height: 1.1 , types: ['bug', 'flying']},
+    {name: 'Golbat', height: 1.6, types: ['poison', 'flying']},
+    {name: 'Slowpoke', height: 1.2, types: ['psychic', 'water']}
+];
