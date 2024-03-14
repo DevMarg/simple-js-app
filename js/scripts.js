006e5a543f60d7cd5e0a40cb37c7a3e27429a939
DevMarg <gurau.margarita@gmail.com>
2024-03-14 15:54:34 +0100

    Rearrange the array
    If applied, the code will look cleaner

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 let pokemonList = [];
 pokemonList = [
-    {name: 'Fearow', height: 1.2 , types: ['normal', 'flying']},
-    {name: 'Butterfree', height: 1.1 , types: ['bug', 'flying']},
-    {name: 'Golbat', height: 1.6, types: ['poison', 'flying']},
-    {name: 'Slowpoke', height: 1.2, types: ['psychic', 'water']}
+    {
+        name: 'Fearow',
+        height: 1.2 ,
+        types: ['normal', 'flying']
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1 ,
+        types: ['bug', 'flying']
+    },
+    {
+        name: 'Golbat',
+        height: 1.6,
+        types: ['poison', 'flying']
+    },
+    {
+        name: 'Slowpoke',
+        height: 1.2,
+        types: ['psychic', 'water']
+    }
 ];
