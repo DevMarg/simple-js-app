22e74bd626ebd503ba244da636abf982b3c3c11b
DevMarg <gurau.margarita@gmail.com>
2024-03-21 16:42:32 +0100

    Update loop and add IIFE
    If applied, this commit will have :
    -forEach function instead of for loop;
    -pokemonList array wrapped in an IIFE;
    -pokemonRepository variable that holds the return of IIFE;
    -IIFE that returns two public functions assigned as keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let pokemonList = [];
-pokemonList = [
+let pokemonRepository = (function(){
+    let pokemonList = [
     {
         name: 'Fearow',
         height: 1.2 ,
@@ -21,11 +21,33 @@ pokemonList = [
         types: ['psychic', 'water']
     }
 ];
-// Display all Pokemon with name and height. Add a comment for the biggest one.
-for (let i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5) {
-        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!`);                
+
+function add(pokemon) {
+    if (typeof pokemon === 'object' && pokemon !== null) {
+        pokemonList.push(pokemon);
     } else {
-        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+        console.error('Error: Invalid type. Only objects can be added to pokemonList')
+    }        
+};
+
+function getAll() {
+    return pokemonList;
+};
+
+return {
+    add: add,
+    getAll: getAll
+};
+
+}) ();
+console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.add({name: "Oddish", height: 0.5, types: ['grass', 'poison']}));
+
+// Display all Pokemon with name and height. Add a comment for the biggest one.
+pokemonRepository.getAll().forEach(function(item){
+    if (item.height > 1.5) {
+        document.write("<p>" + item.name + " " + item.height +  " - Wow, that's big!" + "</p>");                
+    } else {
+        document.write("<p>" + item.name + " " + item.height + "</p>");
     }
-}
+});
